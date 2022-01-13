dbe729d50c6a44c8b6216e26a1f4b74f7f80ffca
Leung <andrew_leung@cable.comcast.net>
2022-01-13 03:33:18 -0500

    init mumbai

--- config.js
+++ config.js
@@ -2,5 +2,4 @@
   export const nftmarketaddress = "0x4c5aA2B4d0Cb19DDB61E0c12a048A826de344281"
   export const nftaddress = "0xF119a05BAA04DB04fa5fCF593C7E9fE713dfDF28"
   export const network_name = "mumbai"
-  export const contract_owner = "0x31b4cE41b12eBb0d1E7beEE20997069EBC20545D"
-  
+  export const contract_owner = "0xE5C9e41248aEe0BA21f60EFA00aEF8aBb68d9d8d"
