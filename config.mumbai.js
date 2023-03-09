b913123f6bb2f1c146f77b41430ebd704a7b468d
Leung <andrew_leung@cable.comcast.net>
2023-03-09 06:24:13 -0500

    greed island mumbai

--- config.mumbai.js
+++ config.mumbai.js
@@ -2,4 +2,4 @@
   export const nftmarketaddress = "0x4c5aA2B4d0Cb19DDB61E0c12a048A826de344281"
   export const nftaddress = "0xF119a05BAA04DB04fa5fCF593C7E9fE713dfDF28"
   export const network_name = "mumbai"
-  export const contract_owner = "0xE5C9e41248aEe0BA21f60EFA00aEF8aBb68d9d8d"
+  export const contract_owner = "0x3B67b2e6806233C79b109577B361E9d35c0cd397"
